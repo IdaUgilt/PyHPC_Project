3c96bceac2d0b3d66b179fdd927ec8019e136194
Rasmus Henriksen <s224399@student.dtu.dk>
2026-04-10 16:31:33 +0200

    task 7. some plot function. 10/04

---
+++ 7/Numba.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#BSUB -J Numba_test"                        # Job name
+#BSUB -q hpc                                # Queue name
+#BSUB -W 10                                 # Wall-clock time
+#BSUB -R "rusage[mem=2GB]"                  # Resources
+#BSUB -o Timer=NumbaCOrrect_%J.out                 # Output file (stdout)
+#BSUB -e Timer=NumbaCOrrect_%J.err                 # Output file (stderr)
+#BSUB -R "select[model==XeonGold6226R]"     # Select CPU model
+#BSUB -n 1                                  # Select number of cores
+#BSUB -R "span[hosts=1]"                    # Specify that the number of cores must be on the same host
+
+# InitializePythonenvironment
+source /dtu/projects/02613_2025/conda/conda_init.sh
+conda activate 02613_2026
+
+echo "test"
+time python3 Jacobi_numba.py 1
+
+# Run Python script
+echo "10"
+for i in 1 2 3; do
+    time python3 Jacobi_numba.py 10
+done
+
+echo "13"
+for i in 1 2 3; do
+    time python3 Jacobi_numba.py 13
+done
+
+echo "16"
+for i in 1 2 3; do
+    time python3 Jacobi_numba.py 16
+done
+
+echo "20"
+for i in 1 2 3; do
+    time python3 Jacobi_numba.py 20
+done
--- Timing/timer.sh
+++ Timing/timer.sh
@@ -1,28 +1,45 @@
 #!/bin/bash
-#BSUB -J timer                          # Job name
+#BSUB -J timer_new                          # Job name
 #BSUB -q hpc                                # Queue name
-#BSUB -W 10                                 # Wall-clock time
-#BSUB -R "rusage[mem=2GB]"                  # Resources
-#BSUB -o Timer=5_%J.out                 # Output file (stdout)
-#BSUB -e Timer=5_%J.err                 # Output file (stderr)
+#BSUB -W 120                                 # Wall-clock time
+#BSUB -R "rusage[mem=25GB]"                  # Resources
+#BSUB -o Timer_new%J.out                 # Output file (stdout)
+#BSUB -e Timer_new%J.err                 # Output file (stderr)
 #BSUB -R "select[model==XeonGold6226R]"     # Select CPU model
 #BSUB -n 1                                  # Select number of cores
 #BSUB -R "span[hosts=1]"                    # Specify that the number of cores must be on the same host
 
 # InitializePythonenvironment
 source /dtu/projects/02613_2025/conda/conda_init.sh
-conda activate 02613
+conda activate 02613_2026
 
 # Run Python script
+echo "5"
+for i in 1 2 3; do
+    time python3 simulate.py 5
+done
+
 echo "10"
-time python3 simulate.py 10
+for i in 1 2 3; do
+    time python3 simulate.py 10
+done
 
-echo "13"
-time simulate.py 13
+echo "12"
+for i in 1 2 3; do
+    time python3 simulate.py 12
+done
 
-echo "16"
-time simulate.py 16
+echo "15"
+for i in 1 2 3; do
+    time python3 simulate.py 15
+done
 
+echo "17"
+for i in 1 2 3; do
+    time python3 simulate.py 17
+done
 
 echo "20"
-time simulate.py 20
+for i in 1 2 3; do
+    time python3 simulate.py 20
+done
